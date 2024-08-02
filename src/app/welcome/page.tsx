f57d3911ea12780011a5fe3228e196023acf2965
pablofretes <pablofretes.balzarelli@gmail.com>
2024-08-02 11:36:25 -0300

    feat: welcome component [WIP]

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,9 @@
+import Welcome from "./components/Welcome"
+
+export default function WelcomePage () {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Welcome/>
+    </div>
+  )
+}
